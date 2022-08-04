12b820e86b3f5064f063666e53676e4b0d385c31
MonsterBlue01 <73154603+MonsterBlue01@users.noreply.github.com>
2022-08-04 15:44:15 -0700

    20220/08/04 16:00

--- Perfect competition.cs
+++ Perfect competition.cs
@@ -2,3 +2,11 @@ Perfect competition:
     - Lots of buyers and sellers
     - Free access to the market
     - Information flow
+
+Monopolistic competition:
+    - Product differentiation
+    - Lots of companies
+    - Free entry or exit of companies (not many barriers to entry)
+    - Each supplier independently decides the market strategy (not a joint monopoly)
+    - Suppliers have relatively high market pricing power
+    - Buyer and seller have incomplete information
