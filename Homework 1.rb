624f9f5a6942163f41a3d2d31f28956cbe661ef4
Blue Monster <wdsrs020808@gmail.com>
2022-09-25 22:17:47 -0700

    2022/09/25 22:30

---
+++ Homework 1.rb
@@ -0,0 +1,16 @@
+Problem 1: x
+Problem 3: x
+Problem 6: a. An event in which an even blue card less than 5 is drawn.
+           b. An event in which a blue card of not less than 5 is drawn.
+           c. Draw a card less than 5 or a blue card or draw an even number of events.
+           d. The event of drawing a blue even-numbered card or drawing an even-numbered card less than 5.
+           e. An event where an odd number of red cards of not less than 5 is selected.
+Problem 7: a. {x < 1 ∪ x > 5}
+           b. {3 < x <= 5}
+           c. {x > 7}
+           d. {3 < x <= 7}
+           e. {Ø}
+Problem 8: A. A = {A}
+           B. B = {B}
+           AB. AB = {A ∪ B}  
+           O. O = {A ∩ B}
