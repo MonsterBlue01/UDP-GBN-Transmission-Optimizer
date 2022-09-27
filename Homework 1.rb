511c6bd7626c6251e67c42667f1b8389e40ddfae
MonsterBlue01 <73154603+MonsterBlue01@users.noreply.github.com>
2022-09-26 20:13:15 -0700

    2022/09/26 20:00

--- Homework 1.rb
+++ Homework 1.rb
@@ -1,5 +1,11 @@
-Problem 1: x
-Problem 3: x
+Problem 1: Assume that x ∈ B^c. We need to show that x ∈ A^c. Let us show this indirectly. By way of contradiction we assume, that x ∈ A. Then x ∈ B because 
+           A ⊂ B. ⇒⇐ (contradiction) because we assumed that x ∈ B^c. Hence x ∈/ A implies x ∈ A^c.
+Problem 3: i. (A U B)^c = A^c ∩ B^c
+              Let P = (A U B)^c and Q = A^c ∩ B^c. Let x be an arbitrary element of P then x ∈ P ⇒ x ∈ (A U B)^c ⇒ x ∉ (A U B) ⇒ x ∉ A and x ∉ B ⇒ x ∈ A^c 
+              and x ∈ B^c ⇒ x ∈ A^c ∩ B^c ⇒ x ∈ Q. Therefore, P ⊂ Q.
+           ii.(A ∩ B)^c = A^c U B^c
+              Let P = (A U B)^c and Q = A^c ∩ B^c. Let y be an arbitrary element of Q then y ∈ Q ⇒ y ∈ A^c ∩ B^c ⇒ y ∈ A^c and y ∈ B^c ⇒ y ∉ A and y ∉ B ⇒ 
+              y ∉ (A U B) ⇒ y ∈ (A U B)^c ⇒ y ∈ P. Therefore, Q ⊂ P.
 Problem 6: a. An event in which an even blue card less than 5 is drawn.
            b. An event in which a blue card of not less than 5 is drawn.
            c. Draw a card less than 5 or a blue card or draw an even number of events.
