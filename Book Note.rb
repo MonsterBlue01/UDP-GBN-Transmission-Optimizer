146fab806ed01101072176db6e7e133e3d0262e2
Blue Monster <wdsrs020808@gmail.com>
2022-09-27 23:45:57 -0700

    2022/09/28 00:00

--- Book Note.rb
+++ Book Note.rb
@@ -1,6 +1,35 @@
 #!/usr/bin/ruby -w
+# -*- coding : utf-8 -*-
  
-puts "Hello, Ruby!";
+print <<EOF
+    这是第一种方式创建here document 。
+    多行字符串。
+EOF
+ 
+print <<"EOF";                # 与上面相同
+    这是第二种方式创建here document 。
+    多行字符串。
+EOF
+ 
+print <<`EOC`                 # 执行命令
+    echo hi there
+    echo lo there
+EOC
+ 
+print <<"foo", <<"bar"          # 您可以把它们进行堆叠
+    I said foo.
+foo
+    I said bar.
+bar
+
+# The output: 这是第一种方式创建here document 。
+# 多行字符串。
+# 这是第二种方式创建here document 。
+# 多行字符串。
+# hi there
+# lo there
+# I said foo.
+# I said bar.
 
 # Definition: 1.3.1:  Experiment and Event. An experiment is any process, real or hypothetical, in which
 #                     the possible outcomes can be identified ahead of time. An event is a well-defined set
@@ -41,14 +70,21 @@ puts "Hello, Ruby!";
 #                     defined to be the set that contains all outcomes that belong both to A and to B. The
 #                     notation for the intersection of A and Bs.
 
-# Theorem:    1.4.1:  Le is A ∩ B
-
 #             1.4.9:  Intersection of Many Sets. The intersection of n sets A1,...,An is defined to be the
 #                     set that contains the elements that are common to all these n sets. The notation for
 #                     this intersection is A1 ∩ A2 ∩ ... ∩ An or ni=1 Ai. Similar notations are used for the
 #                     intersection of an infinite sequence of sets or for the intersection of an arbitrary
 #                     collection of sett A, B, and C be events. Then A ⊂ S. If A ⊂ B and B ⊂ A, then A = B. If A ⊂ B
 #                     and B ⊂ C, then A ⊂ C
+
+#             1.4.10: Disjoint/Mutually Exclusive. It is said that two sets A and B are disjoint, or mutually
+#                     exclusive, if A and B have no outcomes in common, that is, if A ∩ B = ∅. The sets
+#                     A1,...,An or the sets A1, A2,... are disjoint if for every i 	= j , we have that Ai and
+#                     Aj are disjoint, that is, Ai ∩ Aj = ∅ for all i 	= j . The events in an arbitrary collection
+#                     are disjoint if no two events in the collection have any outcomes in common.
+
+# Theorem:    1.4.1:  Let A, B, and C be events. Then A ⊂ S. If A ⊂ B and B ⊂ A, then A = B. If A ⊂ B
+#                     and B ⊂ C, then A ⊂ C.
 
 #             1.4.2:  Let A be an event. Then ∅ ⊂ A.
 
@@ -65,4 +101,14 @@ puts "Hello, Ruby!";
 #             1.4.7:  If A and B are events, then so is A ∩ B. For all events A and B,
 #                     A ∩ B = B ∩ A, A ∩ A = A, A ∩ A^c = ∅, A ∩ ∅=∅, A ∩ S = A. 
 #                     Furthermore, if A ⊂ B, then A ∩ B = A.
+
+#             1.4.8:  Associative Property. For every three events A, B, and C, the following associative
+#                     relations are satisfied: A ∩ B ∩ C = (A ∩ B) ∩ C = A ∩ (B ∩ C)
+
+#             1.4.9:  De Morgan’s Laws. For every two sets A and B, (A ∪ B)^c = A^c ∩ B^c and (A ∩ B)^c = A^c ∪ B^c
+
+#             1.4.10: Distributive Properties. For every three sets A, B, and C, A ∩ (B ∪ C) = (A ∩ B) ∪ (A ∩ C) and A ∪ (B ∩ C) = (A ∪ B) ∩ (A ∪ C).
+
+#             1.4.11: Partitioning a Set. For every two sets A and B, A ∩ B and A ∩ B^c are disjoint and A = (A ∩ B) ∪ (A ∩ B^c).
+#                     In addition, B and A ∩ B^c are disjoint, and A ∪ B = B ∪ (A ∩ B^c).
 
--- Homework 1.rb
+++ Homework 1.rb
@@ -1,22 +1,22 @@
-Problem 1: Assume that x ∈ B^c. We need to show that x ∈ A^c. Let us show this indirectly. By way of contradiction we assume, that x ∈ A. Then x ∈ B because 
-           A ⊂ B. ⇒⇐ (contradiction) because we assumed that x ∈ B^c. Hence x ∈/ A implies x ∈ A^c.
-Problem 3: i. (A U B)^c = A^c ∩ B^c
-              Let P = (A U B)^c and Q = A^c ∩ B^c. Let x be an arbitrary element of P then x ∈ P ⇒ x ∈ (A U B)^c ⇒ x ∉ (A U B) ⇒ x ∉ A and x ∉ B ⇒ x ∈ A^c 
-              and x ∈ B^c ⇒ x ∈ A^c ∩ B^c ⇒ x ∈ Q. Therefore, P ⊂ Q.
-           ii.(A ∩ B)^c = A^c U B^c
-              Let P = (A U B)^c and Q = A^c ∩ B^c. Let y be an arbitrary element of Q then y ∈ Q ⇒ y ∈ A^c ∩ B^c ⇒ y ∈ A^c and y ∈ B^c ⇒ y ∉ A and y ∉ B ⇒ 
-              y ∉ (A U B) ⇒ y ∈ (A U B)^c ⇒ y ∈ P. Therefore, Q ⊂ P.
-Problem 6: a. An event in which an even blue card less than 5 is drawn.
-           b. An event in which a blue card of not less than 5 is drawn.
-           c. Draw a card less than 5 or a blue card or draw an even number of events.
-           d. The event of drawing a blue even-numbered card or drawing an even-numbered card less than 5.
-           e. An event where an odd number of red cards of not less than 5 is selected.
-Problem 7: a. {x < 1 ∪ x > 5}
-           b. {3 < x <= 5}
-           c. {x > 7}
-           d. {3 < x <= 7}
-           e. {Ø}
-Problem 8: A. A = {A}
-           B. B = {B}
-           AB. AB = {A ∪ B}  
-           O. O = {A ∩ B}
+# Problem 1: Assume that x ∈ B^c. We need to show that x ∈ A^c. Let us show this indirectly. By way of contradiction we assume, that x ∈ A. Then x ∈ B because 
+#            A ⊂ B. ⇒⇐ (contradiction) because we assumed that x ∈ B^c. Hence x ∈/ A implies x ∈ A^c.
+# Problem 3: i. (A U B)^c = A^c ∩ B^c
+#               Let P = (A U B)^c and Q = A^c ∩ B^c. Let x be an arbitrary element of P then x ∈ P ⇒ x ∈ (A U B)^c ⇒ x ∉ (A U B) ⇒ x ∉ A and x ∉ B ⇒ x ∈ A^c 
+#               and x ∈ B^c ⇒ x ∈ A^c ∩ B^c ⇒ x ∈ Q. Therefore, P ⊂ Q.
+#            ii.(A ∩ B)^c = A^c U B^c
+#               Let P = (A U B)^c and Q = A^c ∩ B^c. Let y be an arbitrary element of Q then y ∈ Q ⇒ y ∈ A^c ∩ B^c ⇒ y ∈ A^c and y ∈ B^c ⇒ y ∉ A and y ∉ B ⇒ 
+#               y ∉ (A U B) ⇒ y ∈ (A U B)^c ⇒ y ∈ P. Therefore, Q ⊂ P.
+# Problem 6: a. An event in which an even blue card less than 5 is drawn.
+#            b. An event in which a blue card of not less than 5 is drawn.
+#            c. Draw a card less than 5 or a blue card or draw an even number of events.
+#            d. The event of drawing a blue even-numbered card or drawing an even-numbered card less than 5.
+#            e. An event where an odd number of red cards of not less than 5 is selected.
+# Problem 7: a. {x < 1 ∪ x > 5}
+#            b. {3 < x <= 5}
+#            c. {x > 7}
+#            d. {3 < x <= 7}
+#            e. {Ø}
+# Problem 8: A. A = {A}
+#            B. B = {B}
+#            AB. AB = {A ∪ B}  
+#            O. O = {A ∩ B}
