a8f46bee4e28db1622e2f1f76811cfdbb3807063
MonsterBlue01 <73154603+MonsterBlue01@users.noreply.github.com>
2022-10-04 15:29:58 -0700

    2022/10/04 15:30

--- Homework 2.rb
+++ Homework 2.rb
@@ -1,7 +1,14 @@
-Section 1.5: Problems 3, 4 (0.6), 6 (0.5), 7, 9, 11, 14 ((a) Antigen A 
+Section 1.5: Problems 7, 9, 11, 14 ((a) Antigen A 
 0.38 Antigen B
 0.16 (b) 0.04)
 Section 1.6: Problems 1, 3, 4 (1/7), 6 (1/4)
 Section 1.7: Problems 2 (9000), 3, 5, 7, 8 (360/2401)
 Section 1.8: Problems 1, 4 (1/10626), 6 (2/n), 10 (0.1630), 11, 12 
 (0.5798), 17
+
+Section 1.5: 3: a. 1/2
+                b. 1/6
+                c. 3/8
+
+            4: 0.2 + 0.5 - 0.1 = 0.6
+            6: 0.2 + 0.5 - 0.1 - 0.1 = 0.5
