3a45a6d4069652d9fe05834077b67e3e32955fd1
MonsterBlue01 <73154603+MonsterBlue01@users.noreply.github.com>
2022-10-04 09:11:59 -0700

    2022/10/04 09:15

---
+++ Homework 2.rb
@@ -0,0 +1,7 @@
+Section 1.5: Problems 3, 4 (0.6), 6 (0.5), 7, 9, 11, 14 ((a) Antigen A 
+0.38 Antigen B
+0.16 (b) 0.04)
+Section 1.6: Problems 1, 3, 4 (1/7), 6 (1/4)
+Section 1.7: Problems 2 (9000), 3, 5, 7, 8 (360/2401)
+Section 1.8: Problems 1, 4 (1/10626), 6 (2/n), 10 (0.1630), 11, 12 
+(0.5798), 17
