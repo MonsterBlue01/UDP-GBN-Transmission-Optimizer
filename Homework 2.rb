f110bc57ed68e5f494ccc4b60eeeaf1a3d9a4600
MonsterBlue01 <73154603+MonsterBlue01@users.noreply.github.com>
2022-10-04 16:46:42 -0700

    2022/10/04 16:45

--- Homework 2.rb
+++ Homework 2.rb
@@ -1,6 +1,3 @@
-Section 1.5: Problems 7, 9, 11, 14 ((a) Antigen A 
-0.38 Antigen B
-0.16 (b) 0.04)
 Section 1.6: Problems 1, 3, 4 (1/7), 6 (1/4)
 Section 1.7: Problems 2 (9000), 3, 5, 7, 8 (360/2401)
 Section 1.8: Problems 1, 4 (1/10626), 6 (2/n), 10 (0.1630), 11, 12 
@@ -12,3 +9,10 @@ Section 1.5: 3: a. 1/2
 
             4: 0.2 + 0.5 - 0.1 = 0.6
             6: 0.2 + 0.5 - 0.1 - 0.1 = 0.5
+            7:  Min: 0.1 When Pr(A ∪ B) = 1.
+                Max: 0.4. When A ⊂ B.
+            9:  Proved.
+            11. 
+            14. 1): A: 0.38
+                    B: 0.16
+                2): 0.04
