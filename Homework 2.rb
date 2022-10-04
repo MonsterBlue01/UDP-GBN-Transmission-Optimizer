0872e1ee06bf1208b2ff531df7c553f12182c724
Blue Monster <wdsrs020808@gmail.com>
2022-10-03 20:05:40 -0700

    2022/10/03 20:00

--- Homework 2.rb
+++
@@ -1,5 +0,0 @@
-Problem 1: 5 * 4 * 3 * 2 = 120
-Problem 2: 3 * 7 * 5 * 2 = 210
-Problem 3: 9 * 8 = 72
-Problem 3: 
-Problem 4: 8 * 7 * 6 = 336
---
+++ Practice 2.rb
@@ -0,0 +1,7 @@
+Problem 1: 5 * 4 * 3 * 2 = 120
+Problem 2: 3 * 7 * 5 * 2 = 210
+Problem 3: 9 * 8 = 72  (Permutation of 9 distinct objects taken 2 at a time: 9!/(9-2)! = 9!/7! = 9*8)
+Problem 4: 4! + 5! = 1024
+Problem 5: C^8_3 = 8!/3!5! = 56
+Problem 6:Think as if A and B are grouped as one F, then there are 4! ways to permute F, C, D, and E. Moreover, in group F, there are 2! ways
+to permute A and B. So the answer is: 4!x2!
