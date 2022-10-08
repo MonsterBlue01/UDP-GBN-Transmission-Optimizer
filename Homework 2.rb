c9c7986ffac73c0c6ff66fd9daebca26423f0e3b
MonsterBlue01 <73154603+MonsterBlue01@users.noreply.github.com>
2022-10-08 14:50:32 -0700

    2022/10/08 15:00

--- Homework 2.rb
+++ Homework 2.rb
@@ -1,40 +1,40 @@
-Section 1.5: 3: a. 1/2
-                b. 1/6
-                c. 3/8
+# Section 1.5: 3: a. 1/2
+#                 b. 1/6
+#                 c. 3/8
 
-            4: 0.2 + 0.5 - 0.1 = 0.6
-            6: 0.2 + 0.5 - 0.1 - 0.1 = 0.5
-            7:  Min: 0.1 When Pr(A ∪ B) = 1
-                Max: 0.4. When A ⊂ B.
-            9:  Proved.
-            11. 
-            14. 1): A: 0.38
-                    B: 0.16
-                2): 0.04
+#             4: 0.2 + 0.5 - 0.1 = 0.6
+#             6: 0.2 + 0.5 - 0.1 - 0.1 = 0.5
+#             7:  Min: 0.1 When Pr(A ∪ B) = 1
+#                 Max: 0.4. When A ⊂ B.
+#             9:  Proved.
+#             11. 
+#             14. 1): A: 0.38
+#                     B: 0.16
+#                 2): 0.04
 
-Section 1.6: 1: 1/2
-            3: 2/3
-            4: 1/7
-            6: 1/4
+# Section 1.6: 1: 1/2
+#             3: 2/3
+#             4: 1/7
+#             6: 1/4
 
-Section 1.7: 2: 20 * 18 * 25 = 9000
-            3: 5 * 4 * 3 * 2 * 1 = 120
-            5. Now we can choose 4 different numbers from 1, 2, 3, 4, 5, 6 by 6C4 ways. Then we can asign this to four dices in 4! ways. The number of 
-            possibilities of event A is 6C4 * 4! = 6!/4!2! * 4! = 6!/2.
-            P(A) = 6!/2 / 6^4 = 6!/2 / 1296 = 5/18
-            7. Total number of possible cases: 20^12
-            Probability = fovorable / total = 20 * 19 * 18... * 9 / 20^12 = 0.01473
-            8. 5 + 7 - 1 = 11
-            possibility = 7C5 / 11C5
+# Section 1.7: 2: 20 * 18 * 25 = 9000
+#             3: 5 * 4 * 3 * 2 * 1 = 120
+#             5. Now we can choose 4 different numbers from 1, 2, 3, 4, 5, 6 by 6C4 ways. Then we can asign this to four dices in 4! ways. The number of 
+#             possibilities of event A is 6C4 * 4! = 6!/4!2! * 4! = 6!/2.
+#             P(A) = 6!/2 / 6^4 = 6!/2 / 1296 = 5/18
+#             7. Total number of possible cases: 20^12
+#             Probability = fovorable / total = 20 * 19 * 18... * 9 / 20^12 = 0.01473
+#             8. 5 + 7 - 1 = 11
+#             possibility = 7C5 / 11C5
 
-Section 1.8: 1. 20!/(10!10!) = 184756
-            4: Probability that all four bulbs will be defective: 4C4 / 24C4 = (4!/(4!*0!) / (24!/(4!*20!)
-            6: Let's treat A and B as one person. AB can seat as (n - 1)! ways. And they have 2! ways to choose their seats. So the total number of way to 
-            choose the seats are 2!(n - 1)! / n! = 2! / n! = 2 / n
-            10: Total number of ways: 24C10 = 24! / (10! * 14!).
-            The number of ways to select two defective light bulbs with other eight normal bulbs: 2C2 * 22C8 = 319770 / 1961256 = 0.1630
-            11: The number of all ways: 100C12
-            The number of ways to choose 2 people with other people (all 10): 98C10
-            The probability: 98C10 / 100C12 = 0.0133
-            12: (10 2)/(35 2) + (25 2)/(35 2) - (10 2)/(35 2) * (25 2)/(35 2) = 0.5798
-            17: 4(13 4)/(52 4)
+# Section 1.8: 1. 20!/(10!10!) = 184756
+#             4: Probability that all four bulbs will be defective: 4C4 / 24C4 = (4!/(4!*0!) / (24!/(4!*20!)
+#             6: Let's treat A and B as one person. AB can seat as (n - 1)! ways. And they have 2! ways to choose their seats. So the total number of way to 
+#             choose the seats are 2!(n - 1)! / n! = 2! / n! = 2 / n
+#             10: Total number of ways: 24C10 = 24! / (10! * 14!).
+#             The number of ways to select two defective light bulbs with other eight normal bulbs: 2C2 * 22C8 = 319770 / 1961256 = 0.1630
+#             11: The number of all ways: 100C12
+#             The number of ways to choose 2 people with other people (all 10): 98C10
+#             The probability: 98C10 / 100C12 = 0.0133
+#             12: (10 2)/(35 2) + (25 2)/(35 2) - (10 2)/(35 2) * (25 2)/(35 2) = 0.5798
+#             17: 4(13 4)/(52 4)
---
+++ Practice 3.rb
@@ -0,0 +1,12 @@
+Problem 1: (a): Probability that the convict committed armed robbery given that theconvict pushed dope.
+            (b): Probability that the convict did not push dope given that the convict committed armed robbery.
+            (c): Probability that the convict did not commit armed robbery given that the convict did not push dope.
+Problem 2: (a):
+            (b):
+Problem 3:
+Problem 4:
+Problem 5:
+Problem 6:
+Problem 7:
+Problem 8:
+Problem 9:
