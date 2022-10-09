ce8cc7226a88754923a94e116e9e80f0f2e98256
Blue Monster <wdsrs020808@gmail.com>
2022-10-09 11:08:53 -0700

    2022/10/09 11:15

--- Practice 3.rb
+++ Practice 3.rb
@@ -3,7 +3,10 @@ Problem 1: (a): Probability that the convict committed armed robbery given that 
             (c): Probability that the convict did not commit armed robbery given that the convict did not push dope.
 Problem 2: (a): 28/78
             (b): 95/102
-Problem 3:
+Problem 3: P (MM) = 0.4, P (MW) = 0.5 and P (MM|MW) = 0.7
+            (a): P (MM and MW) = 0.5 * 0.7 = 0.35
+            (b): P (MW|MM) = P(MM and MW) / P(MM) = 0.35 / 0.4 = 0.875
+            (c): P (MM or MW) = P (MW) + P (MM) − P (MM and MW) = 0.4 + 0.5 − 0.35 = 0.55.            
 Problem 4:
 Problem 5:
 Problem 6:
