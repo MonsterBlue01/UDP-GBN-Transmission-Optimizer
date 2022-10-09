07ff30d6d478fa8aa950a8354f17f1664b3bbaa7
Blue Monster <wdsrs020808@gmail.com>
2022-10-09 16:45:47 -0700

    2022/10/09 16:45

--- Practice 3.rb
+++ Practice 3.rb
@@ -7,9 +7,15 @@ Problem 3: P (MM) = 0.4, P (MW) = 0.5 and P (MM|MW) = 0.7
             (a): P (MM and MW) = 0.5 * 0.7 = 0.35
             (b): P (MW|MM) = P(MM and MW) / P(MM) = 0.35 / 0.4 = 0.875
             (c): P (MM or MW) = P (MW) + P (MM) − P (MM and MW) = 0.4 + 0.5 − 0.35 = 0.55.            
-Problem 4:
-Problem 5:
-Problem 6:
+Problem 4: According to the question, we can know that P (F_1) = 0.96, P (F_2) = 0.96. 
+            (a): P (F'_1 and F'_2) = (1 - P (F'_1))^2 = (1 - 0.96)^2 = 0.0016
+            (b): 1 - 0.0016 = 0.9984
+Problem 5: P(CF|XR) = P(CF ∪ XR) / P(XR)
+            P(CF ∪ XR) = P(XR) * P(CF|XR)
+            P(TE|XR, CF) = P(TE ∪ XR ∪ CF) / P(XR ∪ CF)
+            P(TE ∪ XR ∪ CF) = P(CF ∪ XR) * P(TE|XR, CF)
+            So the answer is 0.6 * 0.3 * 0.1 = 0.018
+Problem 6: 
 Problem 7:
 Problem 8:
 Problem 9:
