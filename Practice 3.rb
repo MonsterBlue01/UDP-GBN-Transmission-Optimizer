1ac15fbf06f47349a5722a5d48f2c031dde1c800
MonsterBlue01 <73154603+MonsterBlue01@users.noreply.github.com>
2022-10-08 17:32:16 -0700

    2022/10/08 17:30

--- Practice 3.rb
+++ Practice 3.rb
@@ -1,8 +1,8 @@
 Problem 1: (a): Probability that the convict committed armed robbery given that theconvict pushed dope.
             (b): Probability that the convict did not push dope given that the convict committed armed robbery.
             (c): Probability that the convict did not commit armed robbery given that the convict did not push dope.
-Problem 2: (a):
-            (b):
+Problem 2: (a): 28/78
+            (b): 95/102
 Problem 3:
 Problem 4:
 Problem 5:
