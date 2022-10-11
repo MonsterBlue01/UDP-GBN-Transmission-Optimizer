6ef2701fb545974f7caebad153fb674d4f388555
Blue Monster <wdsrs020808@gmail.com>
2022-10-11 10:28:16 -0700

    2022/10/11 10:30

--- Homework 3.rb
+++ Homework 3.rb
@@ -1,4 +1,4 @@
-Section 2.2: 1, 2, 4(0.00463), 7, 10, 11, 12((a) 1/4; (b) 11/24), 13, 23
+Section 2.2: 7, 10, 11, 12((a) 1/4; (b) 11/24), 13, 23
 Section 2.3: 3,4(0.00019),5,6((a) 96/97; (b) 64/65), 7
 
 Section 2.1: 1: Pr(A|B) = Pr(A and B)/Pr(B) = Pr(A)/Pr(B)
@@ -6,3 +6,6 @@ Section 2.1: 1: Pr(A|B) = Pr(A and B)/Pr(B) = Pr(A)/Pr(B)
             3: Pr(A|S) = Pr(A and S)/Pr(S) = Pr(A)/Pr(S) = Pr(A)/1 = Pr(A)
             4: Pr(A1 ∩ A2 ∩ B1 ∩ B2) = P(A1) * P(A2 | A1) * P(B1 | (A1 ∩ A2)) * P(B2 | (A1 ∩ A2 ∩ B1)) = 1/2 * 1/3 * 2/3 * 1/3 = 1/27
             5: Pr = (r / (r + b)) * ((r + k) / (r + b + k)) * ((r + 2k) / (r + b + 2k)) * (b / (r + b + 3k))
+
+Section 2.2: 4: There are six way to get 7: (1,6), (2,5), (3,4), (4,3), (5,2), (6,1). So the probability is 6/36 = 1/6
+            probability for three time: 1/6^3 = 0.00463
