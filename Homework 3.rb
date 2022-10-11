c88a02bf07859f8eda42f0822760f7f6804e779e
MonsterBlue01 <73154603+MonsterBlue01@users.noreply.github.com>
2022-10-10 21:31:43 -0700

    2022/10/10 21:30

--- Homework 3.rb
+++ Homework 3.rb
@@ -1,3 +1,8 @@
-Section 2.1: 1, 2(0), 3, 4(1/27), 5, 6, 9
 Section 2.2: 1, 2, 4(0.00463), 7, 10, 11, 12((a) 1/4; (b) 11/24), 13, 23
 Section 2.3: 3,4(0.00019),5,6((a) 96/97; (b) 64/65), 7
+
+Section 2.1: 1: Pr(A|B) = Pr(A and B)/Pr(B) = Pr(A)/Pr(B)
+            2: Pr(A|B) = Pr(A and B)/Pr(B) = 0/Pr(B) = 0
+            3: Pr(A|S) = Pr(A and S)/Pr(S) = Pr(A)/Pr(S) = Pr(A)/1 = Pr(A)
+            4: Pr(A1 ∩ A2 ∩ B1 ∩ B2) = P(A1) * P(A2 | A1) * P(B1 | (A1 ∩ A2)) * P(B2 | (A1 ∩ A2 ∩ B1)) = 1/2 * 1/3 * 2/3 * 1/3 = 1/27
+            5: Pr = (r / (r + b)) * ((r + k) / (r + b + k)) * ((r + 2k) / (r + b + 2k)) * (b / (r + b + 3k))
