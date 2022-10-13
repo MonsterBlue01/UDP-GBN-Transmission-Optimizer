d84102733f66258f68c53f58e6c60e674d6d62d0
Blue Monster <wdsrs020808@gmail.com>
2022-10-13 12:49:47 -0700

    2022/10/13 12:45

--- Practice 4.rb
+++ Practice 4.rb
@@ -4,9 +4,9 @@ Question 1: (a): 5! / ((5 - 2)! * 2!) * (0.75)^2 * (0.25)^3 = 0.087891
                  P((x = 4) ∨ (x = 5)) = 0.632813
                  P = 1 - 0.632813 = 0.367188
 
-Question 2: (a):
-            (b):
-            (c):
+Question 2: (a): 0.9 * 0.9 * 0.9 * 0.9 = 0.6561
+            (b): 
+            (c): It is asssumed that their winning probability in the playoffs is just the same as that in the regular season, and that the games in the playoffs are independent from each other. 
 
 Question 3: (a):
             (b):
