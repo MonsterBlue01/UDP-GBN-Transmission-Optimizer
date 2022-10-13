c547e1c2652cf75e79a360636fe41695dd914f51
Blue Monster <wdsrs020808@gmail.com>
2022-10-13 09:09:54 -0700

    2022/10/13 09:15

---
+++ Practice 4.rb
@@ -0,0 +1,28 @@
+Question 1: (a): 5! / ((5 - 2)! * 2!) * (0.75)^2 * (0.25)^3 = 0.087891
+            (b): P(x = 4) = 0.395508
+                 P(x = 5) = 0.237305
+                 P((x = 4) ∨ (x = 5)) = 0.632813
+                 P = 1 - 0.632813 = 0.367188
+
+Question 2: (a):
+            (b):
+            (c):
+
+Question 3: (a):
+            (b):
+
+Question 4: 
+
+Question 5: (a):
+            (b):
+
+Question 6: (a):
+            (b):
+
+Question 7: 
+
+Question 8: (a):
+            (b):
+            (c):
+
+Question 9(Optional):
