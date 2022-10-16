cb06ac20d517aa072cbddd4ff40f8d23c17647c1
MonsterBlue01 <73154603+MonsterBlue01@users.noreply.github.com>
2022-10-16 13:34:02 -0700

    2022/10/16 13:30

--- Homework 4.rb
+++ Homework 4.rb
@@ -1,5 +1,4 @@
-Section 3.1: 7,8,10 (problem 1: 6/11; problem 2: 1/15, problem 6: 
-0.1509, problem 8: 0.1330).
+Section 3.1: 8,10 (problem 1: 6/11; problem 2: 1/15, problem 6: 0.1509, problem 8: 0.1330).
 Section 5.2: 8 (0.6993)
 Section 5.3: 2 (0.4266)
 Section 5.4: 3
@@ -16,3 +15,6 @@ Section 3.1: 1: Total samples are = 11 (10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 
 
             6: 15C0 * 0.5^x * 0.5^(15 - 0) + 15C1 * 0.5^x * 0.5^(15 - 1) + 15C2 * 0.5^x * 0.5^(15 - 2) + 
             15C3 * 0.5^x * 0.5^(15 - 3) + 15C4 * 0.5^x * 0.5^(15 - 4) + 15C5 * 0.5^x * 0.5^(15 - 5)
+
+            7: Pr(X ≥ 5) = Pr(Y ≤ 3) = Pr(Y = 0) + Pr(Y = 1) + Pr(Y = 2) + Pr(Y = 3)
+            = P(k;p,n) = (n k)p^k(1 - p)^(n - k)
