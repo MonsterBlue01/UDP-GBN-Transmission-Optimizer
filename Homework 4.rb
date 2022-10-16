49b7dbb5c397cd05688b9958319abe3f4eb85f78
MonsterBlue01 <73154603+MonsterBlue01@users.noreply.github.com>
2022-10-16 12:35:12 -0700

    2022/10/16 12:30

---
+++ Homework 4.rb
@@ -0,0 +1,18 @@
+Section 3.1: 7,8,10 (problem 1: 6/11; problem 2: 1/15, problem 6: 
+0.1509, problem 8: 0.1330).
+Section 5.2: 8 (0.6993)
+Section 5.3: 2 (0.4266)
+Section 5.4: 3
+Section 5.5: 1
+
+Section 3.1: 1: Total samples are = 11 (10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+                Favorable events = 6 (10, 12, 14, 16, 18, 20)
+                Probability that x is even is : = 6/11
+
+            2: {f(x)} = 1, 5(cx) = 1, (c * 1 + c * 2 + ... +c * 5) = 1, c(1 + 2 + 3 + 4 + 5) = 1, c * 15= 1, c = 1/15
+
+            5: P(x) = (Red balls x)(Blue balls 5 - x) / (Total balls Red balls)
+                = (7 x)(3 5 - x) / (x 5)
+
+            6: 15C0 * 0.5^x * 0.5^(15 - 0) + 15C1 * 0.5^x * 0.5^(15 - 1) + 15C2 * 0.5^x * 0.5^(15 - 2) + 
+            15C3 * 0.5^x * 0.5^(15 - 3) + 15C4 * 0.5^x * 0.5^(15 - 4) + 15C5 * 0.5^x * 0.5^(15 - 5)
