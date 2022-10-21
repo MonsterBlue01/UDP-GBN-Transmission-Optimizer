eeff8136bab484b3f5ad1e929bab48afe71ff827
Blue Monster <wdsrs020808@gmail.com>
2022-10-21 14:49:43 -0700

    2022/10/21 14:49

--- Homework 1.rs
+++ Homework 1.rs
@@ -1,1 +1,23 @@
-Section 1.4: Problems 1, 3, 6, 7, 8
+// Section 1.4: Problems 7, 8
+
+// Section 1.4: 6: Q: Suppose that one card is to be selected from a deck of 20 cards that contains 10 red cards numbered from 1 to 10 and 10 
+//                 blue cards numbered from 1 to 10. Let A be the event that a card with an even number is selected, let B be the event that 
+//                 a blue card is selected, and let C be the event that a card with a number less than 5 is selected. Describe the sample space 
+//                 S and describe each of the following events both in words and as subsets of S:
+//                 a. A ∩ B ∩ C 
+//                 b. B ∩ C^c 
+//                 c. A ∪ B ∪ C
+//                 d. A ∩ (B ∪ C) 
+//                 e. A^c ∩ B^c ∩ C^c
+
+//                 A: {2b, 4b}
+//                 B: {5b, 6b, 7b, 8b, 9b, 10b}
+//                 C: {1b, 2b, 3b, 4b, 5b, 6b, 7b, 8b, 9b, 10b, 1r, 2r, 3r, 4r, 5r, 6r, 7r, 8r, 9r, 10r}
+//                 D: {2b, 4b, 6b, 8b, 10b, 2r, 4r}
+//                 E: {5r, 7r, 9r}
+
+
+// print "Hello, World!" to the console
+fn main() {
+    println!("Hello, World!");
+}
