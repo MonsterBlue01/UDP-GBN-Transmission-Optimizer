10ea6baf8178f713a6e6f55eab4284a37a97462e
Blue Monster <wdsrs020808@gmail.com>
2022-10-23 15:52:01 -0700

    2022/10/23 16:00

--- Homework 5.rb
+++ Homework 5.rb
@@ -1,8 +1,3 @@
-Section 3.4: 4,5,8,10 (problem 1: (a) 1/2 (b) 0.75; problem 2: (a) 
-0.27, (b) 0.53, (c) 0.69, (d) 0.3, (e) 0.25; problem 3: (a) 1/40 (b) 1/20 
-(c) 7/40 (d) 0.7; problem 4 (a) 3/2 (b) 3/8 (c) 1/8 (d) 1/2 (e) 0; problem 
-5: (a) 5/4; (b) 79/256 (c) 13/16 (d) 0; problem 8: (a) 5/78; (b) 25/78; 
-(c) y(9+y)/52 (d) (3x^2+2y)/156); problem 10: (b) 1/3)
 Section 3.5: 2, 3, 4, 6, 7, 8, 11 (problem 2: (a) (2x+3)/15 (b) (1+y)/10; 
 problem 3: (b) they are independent; problem 4 (a) f(x)=15 x^2(1-x^2)/4 
 and f(y) = (5(1-y)^{3/2})/2 (b) not independent; problem 7: yes, problem 
@@ -62,4 +57,8 @@ Section 3.4: 1: (a): According to the condition in question, we can get ∫(∫(
             3: (a): Get the ∑, which is 40c. So 40c = 1, c = 1/40
                 (b): 1/40 x |0 - 2| = 1/20
                 (c): 1/40 x 7 = 7/40
+
+            8: (a): F(x, y) = xy(x^2 + y) / 156 for 0 ≤ x ≤ 3, 0 ≤ y ≤ 4
+                P(a ≤ x ≤ b, a ≤ y ≤ b) = F(b, b) + F(a, a) - F(a, b) - F(b, a)
+                = P(1 ≤ x ≤ 2, 1 ≤ y ≤ 2) = F(2, 2) + F(1, 1) - F(1, 2) - F(2, 1) = 0.064
             
