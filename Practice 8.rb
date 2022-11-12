cfbf4f95d089b52a233d0e8dff0074f082a3d08d
MonsterBlue01 <73154603+MonsterBlue01@users.noreply.github.com>
2022-11-12 14:01:29 -0800

    2022/11/12 14:00

---
+++ Practice 8.rb
@@ -0,0 +1,15 @@
+Problem 1: 0.9 is the chance of remaining sunny, 0.1 means the chance of a sunny day becoming cloudy, the 0.5 in the lower left corner represents the chance of a 
+cloudy day becoming sunny, the 0.5 in the lower right corner indicates the chance of continuing to be cloudy
+
+Problem 2: P^2 = [[0.9 0.1][0.5 0.5]] x [[0.9 0.1][0.5 0.5]] = [[0.86 0.14][0.7 0.3]]. So the probability that the weather will change from sunny to rainy in two 
+days will be 0.14
+
+Problem 3: P = [[0.9 0.1][0.5 0.5]], P - I = [[-0.1 0.1][0.5 -0.5]], G = [[-0.1 1][0.5 1]], G^-1 = [[-5/3 5/3][5/6 1/6]], π' = [5/6 1/6].'So in the long run, the 
+chance of a sunny day is 5/6, and the chance of a cloudy day is 1/6.
+
+Problem 4: P = [[0.7 0.3][0.5 0.5]]
+
+Problem 5: P - I = [[-0.3 0.3][0.5 -0.5]], G = [[-0.3 1][0.5 1]], G^-1 = [[-1.25 1.25][0.625 0.325]], π' = [0.625 0.325].'. So in the long run, the probability 
+            of the machine working efficiently is 0.625, and the probability of the machine working inefficiently is 0.325.
+
+Problem 6 & Problem 7 are in the notes.
